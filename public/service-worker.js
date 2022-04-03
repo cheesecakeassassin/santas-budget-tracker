28ed499d2b7b88e58e5875337abc54c41f35338e
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-02 22:00:29 -0400

    fixing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,18 +5,18 @@ const DATA_CACHE_NAME = 'Data-cache-v1';
 const FILES_TO_CACHE = [
     '/',
     './index.html',
-    './manifest.webmanifest',
+    '../manifest.webmanifest',
     './js/index.js',
     './js/idb.js',
     './css/styles.css',
-    './images/icons/icon-96x96.png',
-    './images/icons/icon-72x72.png',
-    './images/icons/icon-128x128.png',
-    './images/icons/icon-144x144.png',
-    './images/icons/icon-152x152.png',
-    './images/icons/icon-192x192.png',
-    './images/icons/icon-384x384.png',
-    './images/icons/icon-512x512.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
 ];
 
 // Respond with cached resources
