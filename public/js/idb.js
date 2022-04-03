d849c3bcca78bb9df85310f9d898d14bf89ed1f9
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-02 21:41:24 -0400

    working on idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,1 +1,26 @@
+// Creates temp database variable for indexedDB
 let db;
+
+// Contains request response upon opening the db
+const request = indexedDB.open("budget_tracker", 1);
+
+// Stores new budget into indexedDB
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_budget", { autoIncrement: true });
+};
+
+// Runs when indexedDB sucessfully opens
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // Checks if online to upload the current budget
+  if (navigator.onLine) {
+    uploadBudgets();
+  }
+};
+
+// Console logs an error if request fails
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
